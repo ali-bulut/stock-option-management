91d123f630afea3639d920ee3a63bea4d4811269
Ali Bulut <ali@alibulut.net>
2024-04-01 21:25:49 +0300

    WEB: add STRIPE_PUBLIC_KEY to EnvironmentHelper

--- web/helpers/EnvironmentHelper.ts
+++ web/helpers/EnvironmentHelper.ts
@@ -5,6 +5,7 @@ import { AxiosRequestConfig } from "axios";
  */
 export default class EnvironmentHelper {
   public static API_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
+  public static STRIPE_PUBLIC_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!;
 
   public static defaultAxiosRequestConfiguration = (): AxiosRequestConfig => ({
     // TODO: change it to https
