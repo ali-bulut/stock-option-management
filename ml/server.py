891f560e49c7e9838cb3b9a829466efc2bb41325
Ali Bulut <ali@alibulut.net>
2024-04-27 00:26:42 +0300

    ML: remove tickers endpoint

--- ml/server.py
+++ ml/server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from multiprocessing import Process
 from helpers.simulation_helper import simulate
-from helpers.tickers_helper import retrieve_tickers
 from helpers.trade_helper import trade
 from dotenv import load_dotenv
 import os
@@ -11,11 +10,11 @@ load_dotenv()
 
 @app.route('/stock_simulations', methods=['POST'])
 def stock_simulations():
-    tickers = request.get_json().get('tickers')
+    stock_options = request.get_json().get('stock_options')
     initial_amount = request.get_json().get('initial_amount')
     auth_token = request.get_json().get('auth_token')
 
-    p = Process(target=simulate, args=(initial_amount, tickers, auth_token))
+    p = Process(target=simulate, args=(initial_amount, stock_options, auth_token))
     p.daemon = True
     p.start()
 
@@ -35,13 +34,5 @@ def stock_trades():
 
     return { 'message': 'Trade started' }
 
-@app.route('/tickers', methods=['GET'])
-def tickers():
-    search_param = request.args.get('search')
-    if search_param is None:
-        search_param = ''
-
-    return retrieve_tickers(search_param)
- 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 33507)))
