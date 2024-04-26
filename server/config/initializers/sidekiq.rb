020f419c500dde51646942d20cfc0159d891161b
Ali Bulut <ali@alibulut.net>
2024-04-26 23:10:57 +0300

    SERVER: remove redundant methods from sidekiq initializer

--- server/config/initializers/sidekiq.rb
+++ server/config/initializers/sidekiq.rb
@@ -14,27 +14,8 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == [ENV.fetch('SIDEKIQ_WEB_USERNAME'), ENV.fetch('SIDEKIQ_WEB_PASSWORD')]
 end
 
-# ======================================================= Sidekiq Crons ================================================
-
 if Sidekiq.server?
-  utc_file = "config/schedule.yml"
+  cron_schedule_file = "config/schedule.yml"
 
-  def _cron_set_to_timezone(data, timezone = 'UTC')
-    data.each { |_, properties| properties['cron'] = "#{properties['cron']} #{timezone}" }
-  end
-
-  def _cron_parse_crons(data)
-    data.each { |_, properties| properties['cron'] = Fugit.parse(properties['cron']).original }
-  end
-
-  def _cron_load_schedule_file(path)
-    data = YAML.load_file(path) || {}
-    _cron_parse_crons(data)
-    data
-  end
-
-  schedules = _cron_load_schedule_file(utc_file)
-  schedules = _cron_set_to_timezone(schedules, 'UTC')
-
-  Sidekiq::Cron::Job.load_from_hash!(schedules || {})
+  Sidekiq::Cron::Job.load_from_hash(YAML.load_file(cron_schedule_file))
 end
