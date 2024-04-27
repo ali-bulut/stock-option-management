7b780688a6649aaa10dad73e45859fa5591083ad
Ali Bulut <ali@alibulut.net>
2024-04-27 18:43:59 +0300

    SERVER: enable trading for regular stocks only on trading hours

--- server/app/jobs/trade_plan_job.rb
+++ server/app/jobs/trade_plan_job.rb
@@ -4,7 +4,7 @@ class TradePlanJob < ApplicationJob
 
   def perform
     TradePlan.active.find_each do |trade_plan|
-      stock_options = trade_plan.trade_plan_stock_options.map do |trade_plan_stock_option|
+      stock_options = trade_plan.trade_plan_stock_options.available_for_trade.map do |trade_plan_stock_option|
         {
           stock_option_symbol: trade_plan_stock_option.stock_option.symbol,
           quantity: trade_plan_stock_option.quantity,
--- server/app/models/trade_plan_stock_option.rb
+++ server/app/models/trade_plan_stock_option.rb
@@ -7,6 +7,12 @@ class TradePlanStockOption < ActiveRecord::Base
   validates_uniqueness_of :stock_option_id, scope: :trade_plan_id
   validates_numericality_of :quantity, greater_than_or_equal_to: 0
 
+  scope :available_for_trade, -> do
+    is_market_open = Time.current.in_time_zone("America/New_York").hour.between?(10, 16)
+    is_market_open ? all : joins(:stock_option).where(stock_options: { partial_buy: true })
+                                               .or(where(stock_option_id: StockOption.cash.id))
+  end
+
   def self.by_symbol(symbol)
     joins(:stock_option).where(stock_options: { symbol: symbol }).first
   end
